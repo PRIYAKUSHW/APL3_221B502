fb097a7b317b14f9c6f237bc48dd0daef190d9f0
221B502 <221B502@juet.co.in>
2025-03-28 14:20:24 +0530

    Adding Lab5

---
+++ LAB5/Rum.java
@@ -0,0 +1,9 @@
+class Rum extends Beverage{
+    protected void addContinen(){
+        System.out.println("Add Water.");
+        
+    }
+    void stir(){
+        System.out.println("Dtir for 35 seconds..");
+    }
+}
